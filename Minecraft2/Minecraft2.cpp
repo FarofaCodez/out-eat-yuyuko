0265c55777070727f63b79857f844b57f79fbbfc
FarofaCodez <anaj42863@outlook.com>
2025-04-14 19:44:13 -0300

    add initial code

---
+++ Minecraft2/Minecraft2.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <Windows.h>
+#pragma comment(lib, "comctl32.lib")
+#include <commctrl.h>
+#define IDB_IMAGE1 101
+#define IDB_IMAGE2 102
+
+LRESULT wndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+HBITMAP rumia = NULL;
+HBITMAP yuyuko = NULL;
+HINSTANCE hInst_g = NULL;
+int score = 0;
+int yuyuScore = 0;
+
+int WinMain(_In_ HINSTANCE hInst, _In_opt_ HINSTANCE hPrevInst, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+	WNDCLASS wndClass = {};
+	wndClass.lpfnWndProc = wndProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInst;
+	wndClass.hIcon = NULL;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)(COLOR_BACKGROUND) + 1;
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = "BakaWndClass";
+
+	hInst_g = hInst;
+
+	RegisterClassA(&wndClass);
+
+	HWND hWnd = CreateWindowExA(
+		0,
+		"BakaWndClass",
+		"Out-Eat Yuyuko",
+		WS_TILEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720,
+		NULL, NULL, NULL, NULL
+	);
+
+	INITCOMMONCONTROLSEX commonInit = {};
+	commonInit.dwICC = ICC_WIN95_CLASSES;
+	commonInit.dwSize = sizeof(INITCOMMONCONTROLSEX);
+
+	InitCommonControlsEx(&commonInit);
+
+	HWND eatBtn = CreateWindowEx(
+		0,
+		"BUTTON",
+		"Eat",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		50,
+		550,
+		300,
+		50,
+		hWnd,
+		(HMENU)1,
+		NULL,
+		NULL
+	);
+
+	MSG msg;
+	BOOL bRet;
+	while ((bRet = GetMessageA(&msg, hWnd, 0, 0)) != 0) {
+		if (bRet == -1) {
+			return 0;
+		}
+		else {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+}
+
+bool drawBitmap(HDC hdc, HBITMAP image, int x, int y) {
+	if (!image) return false;
+	HDC hdcMem = CreateCompatibleDC(hdc);
+	HBITMAP hOldBmp = (HBITMAP)SelectObject(hdcMem, image);
+	BITMAP bmp;
+	GetObject(image, sizeof(BITMAP), &bmp);
+	BitBlt(hdc, x, y, bmp.bmWidth, bmp.bmHeight, hdcMem, 0, 0, SRCCOPY);
+	SelectObject(hdcMem, hOldBmp);
+	DeleteDC(hdcMem);
+	return true;
+}
+
+LRESULT wndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+
+	case WM_CREATE:
+		yuyuko = LoadBitmap(hInst_g, MAKEINTRESOURCE(IDB_IMAGE1));
+		rumia = LoadBitmap(hInst_g, MAKEINTRESOURCE(IDB_IMAGE2));
+		SetTimer(hWnd, 0, 500, NULL);
+
+		break;
+
+	case WM_PAINT:{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+
+		drawBitmap(hdc, rumia, 50, 200);
+		drawBitmap(hdc, yuyuko, 800, 200);
+
+
+
+		EndPaint(hWnd, &ps);
+
+		break;
+	}
+	
+	case WM_COMMAND:
+		if (wParam == 1) {
+			score += 2;
+		}
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		
+		break;
+
+	case WM_TIMER:
+		yuyuScore += 5;
+		char text[300] = "Out-Eat Yuyuko";
+		sprintf_s(text, "Out-Eat Yuyuko -- You: %d, Yuyuko: %d", score, yuyuScore);
+		SetWindowText(hWnd, text);
+	}
+
+	return ::DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
